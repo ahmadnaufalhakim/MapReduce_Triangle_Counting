16096927bff5e731986419b753a23ee11099f6f3
gamapradiptawirawan <gamapradipta88@gmail.com>
2020-11-30 21:51:02 +0700

    typo

--- TriangleCounting.java
+++ TriangleCounting.java
@@ -42,7 +42,7 @@ public class TriangleCounting extends Configured implements Tool {
                     int compare = valuesCopy.get(u).compareTo(valuesCopy.get(w));
                     if (compare > 0) {
                         // Format key value -> 1 2,3
-                        context.write(new Text(key.toString()), new Text(valuesCopy.get(u).toString() + "," + valuesCopy.get(w).toString()));
+                        context.write(new Text(key.toString()), new Text(valuesCopy.get(w).toString() + "," + valuesCopy.get(u).toString()));
                     }
                 }
             }
