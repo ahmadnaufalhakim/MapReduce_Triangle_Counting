2951dd4756d28cdce1eefacdfbd6b8bc7740db06
Ahmad Naufal Hakim <ahmadnaufalhakim@gmail.com>
2020-11-30 20:59:40 +0700

    change directory

--- run.sh
+++ run.sh
@@ -5,8 +5,8 @@
 rm TriangleCounting.jar
 rm TriangleCounting*.class
 
-/usr/local/hadoop/bin/hadoop com.sun.tools.javac.Main src/TriangleCounting.java
-jar cf TriangleCounting.jar src/TriangleCounting*.class
+/usr/local/hadoop/bin/hadoop com.sun.tools.javac.Main TriangleCounting.java
+jar cf TriangleCounting.jar TriangleCounting*.class
 
 /usr/local/hadoop/bin/hadoop jar TriangleCounting.jar TriangleCounting $1 $2
 /usr/local/hadoop/bin/hdfs dfs -cat /home/ubuntu/MapReduce_Triangle_Counting/output/*
